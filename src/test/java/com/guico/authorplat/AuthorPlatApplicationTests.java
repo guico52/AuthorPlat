874e2f0a9327bae7260bec68347c7d16dd7a1a02
guico52 <1305487489@qq.com>
2023-03-11 23:10:06 +0800

    Initial commit

---
+++ src/test/java/com/guico/authorplat/AuthorPlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guico.authorplat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorPlatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
